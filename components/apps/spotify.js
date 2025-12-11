1b6a0a515ee7c39061bb511ab38bfc3ad684e7df
badaouiadampro-cpu <badaoui.adam.pro@gmail.com>
2025-12-11 16:29:21 +0100

    Update spotify.js

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZEVXbKOefHPXPMyf" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZEVXbJU9eQpX8gPT" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
